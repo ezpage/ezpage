2d7f3374826612127dddd524568f2c52bc173fa5
haonx <qtvhao@gmail.com>
2017-05-29 21:55:11 +0700

    init Builder

---
+++ Builder.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qtvha
+ * Date: 5/29/2017
+ * Time: 9:48 PM
+ */
+
+namespace Ezpage\Ezpage;
+
+
+class Builder
+{
+    public function renderExample()
+    {
+        return '';
+    }
+}
